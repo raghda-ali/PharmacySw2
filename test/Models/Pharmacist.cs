e16de89f9250cee3c7a8be098b550fd43258a175
raghda-ali <raghdaali991@gmail.com>
2020-05-15 00:24:57 +0200

    add pharmacist and medicine table

---
+++ test/Models/Pharmacist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test.Models
+{
+    public class Pharmacist
+    {
+        public int id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string EmailID { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Boolean IsEmailVerified { get; set; }
+        public Guid ActivationCode { get; set; }
+    }
+}
