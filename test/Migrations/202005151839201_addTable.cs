69b1487c6113ea6b23d62c65a339e158f5b825b2
raghda-ali <raghdaali991@gmail.com>
2020-05-15 20:45:09 +0200

    .

---
+++ test/Migrations/202005151839201_addTable.cs
@@ -0,0 +1,33 @@
+﻿namespace test.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addTable : DbMigration
+    {
+        public override void Up()
+        {
+            DropTable("dbo.Pharmacists");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Pharmacists",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        Firstname = c.String(),
+                        Lastname = c.String(),
+                        Username = c.String(),
+                        Password = c.String(),
+                        EmailID = c.String(),
+                        DateOfBirth = c.DateTime(nullable: false),
+                        IsEmailVerified = c.Boolean(nullable: false),
+                        ActivationCode = c.Guid(nullable: false),
+                    })
+                .PrimaryKey(t => t.id);
+            
+        }
+    }
+}
--- test/Models/Pharmacists.cs
+++ test/Models/Pharmacists.cs
@@ -12,6 +12,9 @@ namespace test.Models
         public string Password { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
-        public string Email { get; set; }
+        public string EmailID { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Boolean IsEmailVerified { get; set; }
+        public Guid ActivationCode { get; set; }
     }
 }
