f608f468ebb5e2275fd21eef620f3a74bfc2af16
raghda-ali <raghdaali991@gmail.com>
2020-05-15 16:41:10 +0200

    add login view

---
+++ test/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+        //    string btnClick = Request["loginbtn"];
+        //    if(btnClick == "Login")
+        //    {
+        //        string userName = Request["userName"];
+        //        string password = Request["password"];
+
+        //    }
+        //    return View();
+        //}
+        //[HttpPost]
+        //public ActionResult Autherize()
+        //{
+
+        //}
+    }
+}
